de67e4b484bef153a887df97e79959e5d0e44571
AlbertBaluyot <125802104+Uzumakinaruto08@users.noreply.github.com>
2026-02-13 21:09:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let highestZ = 1;
 
 class Paper {
   holdingPaper = false;
-  mouseTouchX = 0;
-  mouseTouchY = 0;
+  touchStartX = 0;
+  touchStartY = 0;
   mouseX = 0;
   mouseY = 0;
   prevMouseX = 0;
@@ -16,26 +16,14 @@ class Paper {
   rotating = false;
 
   init(paper) {
-    document.addEventListener('mousemove', (e) => {
+    // Shared move logic for both Mouse and Touch
+    const handleMove = (clientX, clientY) => {
       if(!this.rotating) {
-        this.mouseX = e.clientX;
-        this.mouseY = e.clientY;
+        this.mouseX = clientX;
+        this.mouseY = clientY;
         
         this.velX = this.mouseX - this.prevMouseX;
         this.velY = this.mouseY - this.prevMouseY;
-      }
-        
-      const dirX = e.clientX - this.mouseTouchX;
-      const dirY = e.clientY - this.mouseTouchY;
-      const dirLength = Math.sqrt(dirX*dirX+dirY*dirY);
-      const dirNormalizedX = dirX / dirLength;
-      const dirNormalizedY = dirY / dirLength;
-
-      const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
-      let degrees = 180 * angle / Math.PI;
-      degrees = (360 + Math.round(degrees)) % 360;
-      if(this.rotating) {
-        this.rotation = degrees;
       }
 
       if(this.holdingPaper) {
@@ -43,31 +31,57 @@ class Paper {
           this.currentPaperX += this.velX;
           this.currentPaperY += this.velY;
         }
+        
         this.prevMouseX = this.mouseX;
         this.prevMouseY = this.mouseY;
 
         paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
       }
-    })
+    }
+
+    // --- MOUSE EVENTS ---
+    document.addEventListener('mousemove', (e) => {
+      handleMove(e.clientX, e.clientY);
+    });
 
     paper.addEventListener('mousedown', (e) => {
       if(this.holdingPaper) return; 
       this.holdingPaper = true;
-      
       paper.style.zIndex = highestZ;
       highestZ += 1;
       
       if(e.button === 0) {
-        this.mouseTouchX = this.mouseX;
-        this.mouseTouchY = this.mouseY;
-        this.prevMouseX = this.mouseX;
-        this.prevMouseY = this.mouseY;
+        this.prevMouseX = e.clientX;
+        this.prevMouseY = e.clientY;
       }
       if(e.button === 2) {
         this.rotating = true;
       }
     });
+
+    // --- TOUCH EVENTS (MOBILE) ---
+    paper.addEventListener('touchstart', (e) => {
+      if(this.holdingPaper) return; 
+      this.holdingPaper = true;
+      paper.style.zIndex = highestZ;
+      highestZ += 1;
+      
+      this.prevMouseX = e.touches[0].clientX;
+      this.prevMouseY = e.touches[0].clientY;
+    });
+
+    paper.addEventListener('touchmove', (e) => {
+      // This line is key: it stops the mobile page from scrolling while dragging
+      e.preventDefault(); 
+      handleMove(e.touches[0].clientX, e.touches[0].clientY);
+    }, { passive: false });
+
     window.addEventListener('mouseup', () => {
+      this.holdingPaper = false;
+      this.rotating = false;
+    });
+
+    window.addEventListener('touchend', () => {
       this.holdingPaper = false;
       this.rotating = false;
     });
